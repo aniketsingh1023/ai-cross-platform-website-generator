b6663f9a17202137d97786b08930fa87d366bf1b
aniketsingh1023 <aniketsinghn10@gmail.com>
2026-01-04 18:07:28 +0530

    [ADD] added logout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import UserButton from "@/features/auth/components/user-button";
 
 export default function Home() {
   return (
    <div>
     <h1 className = "text-3xl font-bold text-rose-500">this is a button</h1>
-    <Button>Jara Jara Click ME </Button>
+    <UserButton />
    </div>
   );
 }
