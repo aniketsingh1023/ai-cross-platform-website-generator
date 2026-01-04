f1d45656424d2dfb94e1e9f4482f8408097689bb
aniketsingh1023 <aniketsinghn10@gmail.com>
2026-01-04 19:28:07 +0530

    [ADD]added landing page with header and footer both

---
+++ components/providers/theme-providers.tsx
@@ -0,0 +1,15 @@
+"user client"
+
+import * as React from "react";
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+export function ThemeProviders({
+    children,
+    ...props
+}: React.ComponentProps<typeof NextThemeProvider>) {
+    return (
+        <NextThemeProvider {...props}>
+            {children}
+        </NextThemeProvider>
+    );
+}
